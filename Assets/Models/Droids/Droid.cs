5f22a3ae79bd2467138d75b538ce6a3f9f44538f
Cade Saling <csaling@muskingum.edu>
2020-09-01 13:15:38 -0400

    Just Finsihed Video 8

---
+++ Assets/Models/Droids/Droid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Droid : MonoBehaviour
+{
+    [SerializeField] private float spawnRate = 0.10f;
+    [SerializeField] private float catchRate = 0.10f;
+    [SerializeField] private int attack = 0;
+    [SerializeField] private int defence = 0;
+    [SerializeField] private int hp = 10;
+
+    public float SpawnRate
+    {
+        get { return spawnRate; }
+    }
+    
+    public float Catchrate
+    {
+        get { return catchRate; }
+    }
+    
+    public int Attack
+    {
+        get { return attack; }
+    }
+    
+    public int Defence
+    {
+        get { return defence; }
+    }
+    
+    public int Hp
+    {
+        get { return hp; }
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
