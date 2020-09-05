918e45627a87117465b537711d385f93a0fb5671
Cade Saling <csaling@muskingum.edu>
2020-09-04 21:04:44 -0400

    Fixed Huge Error, Don't Go Back

---
+++ Assets/Models/Field Objects/POI XP Bonus/XPBonus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XPBonus : MonoBehaviour
+{
+    [SerializeField] private int bonus = 10;
+
+    private void OnMouseDown()
+    {
+        GameManager.Instance.CurrentPlayer.addXp(bonus);
+        Destroy(gameObject);
+    }
+}
