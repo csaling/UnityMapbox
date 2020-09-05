0c1c485750208ecfe0c296a2aaf6f23aae7c4c87
Cade Saling <csaling@muskingum.edu>
2020-09-04 20:58:29 -0400

    Fixed Huge Error, Don't Go Back

--- Assets/Models/Player/Scripts/Player.cs
+++ Assets/Models/Player/Scripts/Player.cs
@@ -42,7 +42,7 @@ public class Player : MonoBehaviour
 
     public void addXp(int xp)
     {
-        xp = Math.Max(0, xp);
+        this.xp = Math.Max(0, xp);
     }
 
     public void AddDroid(GameObject droid)
