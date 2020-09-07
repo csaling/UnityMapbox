9130cd634ed9ccc4e90883eb00a7d9e5dfa34550
Cade Saling <csaling@muskingum.edu>
2020-09-07 19:03:06 -0400

    Finished Video 17

---
+++ Assets/Scenes/Capture/CaptureSceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureSceneManager : PocketDroidsSceneManager
+{
+    public override void playerTapped(GameObject player)
+    {
+        print("CaptureSceneManager.playerTapped activated");
+    }
+
+    public override void droidTapped(GameObject droid)
+    {
+        print("CaptureSceneManager.droidTapped activated");
+    }
+}
