130f6ed59ca0a26d95778f79a8acbf062daab532
Cade Saling <csaling@muskingum.edu>
2020-09-10 20:37:53 -0400

    Project Completed

---
+++ Assets/Models/Droids/DroidData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class DroidData
+{
+    private float spawnRate;
+    private float catchRate;
+    private int attack;
+    private int defense;
+    private int hp;
+    private string crySound;
+
+    public float SpawnRate
+    {
+        get { return spawnRate; }
+    }
+    
+    public float CatchRate
+    {
+        get { return catchRate; }
+    }
+
+    public int Attack
+    {
+        get { return attack; }
+    }
+    
+    public int Defense
+    {
+        get { return defense; }
+    }
+    
+    public int Hp
+    {
+        get { return hp; }
+    }
+
+    public string CrySound
+    {
+        get { return crySound; }
+    }
+
+    public DroidData(Droid droid)
+    {
+        spawnRate = droid.SpawnRate;
+        catchRate = droid.Catchrate;
+        attack = droid.Attack;
+        defense = droid.Defense;
+        hp = droid.Hp;
+        AudioClip clip;
+        crySound = droid.CrySound.name;
+    }
+}
